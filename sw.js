7571c2be92cf1b3d9c6554410a3a0a4e63a990d4
EdselGitHub <149871435+EdselGitHub@users.noreply.github.com>
2025-03-24 08:27:10 +0700

    done

--- sw.js
+++ sw.js
@@ -1,73 +1,45 @@
-const CACHE_NAME = 'pwa-cache-v3';
-const DYNAMIC_CACHE = 'pwa-dynamic-cache';
-const OFFLINE_PAGE = '/offline.html';
+const CACHE_NAME = "blog-cache-v1";
+const urlsToCache = [
+    "index.html",
+    "styles.css",
+    "offline.html",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png"
+];
 
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing...');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Pre-caching assets');
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/offline.html',
-        '/src/css/app.css',
-        '/src/js/app.js',
-        '/manifest.json',
-        '/second.html',
-        '/src/css/offline.css'
-      ]).then(() => {
-        console.log('[Service Worker] All assets cached');
-      }).catch((error) => {
-        console.error('[Service Worker] Failed to cache assets', error);
-      });
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating...');
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME && key !== DYNAMIC_CACHE) {
-            console.log('[Service Worker] Deleting old cache:', key);
-            return caches.delete(key);
-          }
+// Install Service Worker
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
         })
-      );
-    })
-  );
-  return self.clients.claim();
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Return cached response if found
-      if (response) {
-        return response;
-      }
-      
-      // Otherwise, fetch from network
-      return fetch(event.request).then(function(res) {
-        // Make a copy of the response
-        const resClone = res.clone();
-        
-        // Open dynamic cache and store the response
-        caches.open(DYNAMIC_CACHE).then(function(cache) {
-          cache.put(event.request, resClone);
-        });
-        
-        return res;
-      }).catch(function(err) {
-        // If both cache and network fail for HTML requests, show offline page
-        if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match(OFFLINE_PAGE);
-        }
-      });
-    })
-  );
+// Fetching strategy: Network then Cache
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        fetch(event.request).then(response => {
+            return caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        }).catch(() => {
+            return caches.match(event.request).then(response => {
+                return response || caches.match("offline.html");
+            });
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+                .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
 });
